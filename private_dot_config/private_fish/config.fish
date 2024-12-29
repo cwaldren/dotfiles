91f274b80c96b9f9b13637abda7a8d46879d8a8f
Casey Waldren <caseywaldren@gmail.com>
2024-12-29 15:25:44 -0800

    add fish config, use lazyvim config

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    alias vimrc="nvim ~/.vimrc"
+    alias nvimrc="nvim ~/.config/nvim/init.lua"
+    alias fishrc="nvim ~/.config/fish/config.fish"
+    alias zshrc="nvim ~/.zshrc"
+end
